02af9f3b788f6d85f129844a30546f24c61ca591
Sama-E <112719147+Sama-E@users.noreply.github.com>
2023-09-25 21:57:59 -0400

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Footer from './components/Global/Footer';
+import LandingPg from './pages/LandingPg/LandingPg';
+
+function App() {
+
+  return (
+    <>
+      <LandingPg />
+      <Footer />
+    </>
+  )
+}
+
+export default App;
---
+++ client/src/pages/LandingPg/LandingPg.jsx
@@ -0,0 +1,12 @@
+import Hero from "/src/components/LandingPg/Hero";
+
+
+const LandingPg = () => {
+  return (
+    <>
+      <Hero />
+    </>
+  )
+}
+
+export default LandingPg;
