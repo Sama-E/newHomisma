67f44924f8e2776d29903fafc4ec56c4af03023b
Sama-E <112719147+Sama-E@users.noreply.github.com>
2023-09-28 11:35:34 -0400

    register/login

--- client/src/pages/Auth/Login.jsx
+++ client/src/pages/Auth/Login.jsx
@@ -1,35 +1,99 @@
-import React, { useState } from "react";
-import { useContext } from "react";
+import React, { useState, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { Box, styled, Typography } from "@mui/material";
+
+import CustomButton from "/src/components/Custom/CustomButton";
 // import { AuthContext } from "/src/context/AuthContext";
 
 const Login = () => {
+
+  /* STYLE */
+  const CustomBox = styled(Box)(({ theme }) => ({
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: theme.spacing(5),
+    marginTop: theme.spacing(3),
+    [theme.breakpoints.down("md")]: {
+      flexDirection: "column",
+      alignItems: "center",
+      textAlign: "center",
+    },
+  }));
+
+  const FormBox = styled(Box)(({ theme }) => ({
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 50,
+    gap: theme.spacing(5),
+    marginTop: theme.spacing(3),
+    [theme.breakpoints.down("md")]: {
+      flexDirection: "column",
+      alignItems: "center",
+      textAlign: "center",
+    },
+  }));
+
+  /* FUNCTION */
+  // const [inputs, setInputs] = useState({
+  //   email: "",
+  //   password: "",
+  // });
+  // const [err, setError] = useState(null);
+
+  // const navigate = useNavigate();
+
+  // const { login } = useContext(AuthContext);
+
+
+  // const handleChange = (e) => {
+  //   setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  // };
+
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   try {
+  //     await login(inputs)
+      
+  //     navigate("/");
+  //   } catch (err) {
+  //     setError(err.response.data);
+  //   }
+  // };
+
   return (
-    <div className="auth">
-    <form>
-      <h1>Login</h1>
-      <input
-        required
-        type="email"
-        placeholder="Email"
-        name="email"
-        // onChange={handleChange}
-      />
-      <input
-        required
-        type="password"
-        placeholder="Password"
-        name="password"
-        // onChange={handleChange}
-      />
-      {/* <button onClick={handleSubmit}>Login</button> */}
-      <button>Login</button>
-      {/* {err && <p>{err}</p>} */}
-      <span>
-        Don't you have an account? <Link to="/register">Register</Link>
-      </span>
-    </form>
-  </div>
+    <CustomBox>
+      <FormBox>
+        <h1>Login</h1>
+        <input
+          required
+          type="email"
+          placeholder="Email"
+          name="email"
+          // onChange={handleChange}
+        />
+        <input
+          required
+          type="password"
+          placeholder="Password"
+          name="password"
+          // onChange={handleChange}
+        />
+        {/* <button onClick={handleSubmit}>Login</button> */}
+        <CustomButton
+          backgroundColor="#0F1B4C"
+          color="#fff"
+          buttonText="Login"
+          heroBtn={true}
+        />
+        {/* {err && <p>{err}</p>} */}
+        <span>
+          Don't you have an account? <Link to="/register">Register</Link>
+        </span>
+      </FormBox>
+    </CustomBox>
   )
 }
 
--- client/src/pages/Auth/Register.jsx
+++ client/src/pages/Auth/Register.jsx
@@ -1,8 +1,120 @@
-import React from 'react'
+import { Box, styled, Typography } from "@mui/material";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+// import axios from "axios";
+
+import CustomButton from "/src/components/Custom/CustomButton";
 
 const Register = () => {
+
+  /* STYLE */
+  const CustomBox = styled(Box)(({ theme }) => ({
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: theme.spacing(5),
+    marginTop: theme.spacing(3),
+    [theme.breakpoints.down("md")]: {
+      flexDirection: "column",
+      alignItems: "center",
+      textAlign: "center",
+    },
+  }));
+
+  const FormBox = styled(Box)(({ theme }) => ({
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 50,
+    gap: theme.spacing(5),
+    marginTop: theme.spacing(3),
+    [theme.breakpoints.down("md")]: {
+      flexDirection: "column",
+      alignItems: "center",
+      textAlign: "center",
+    },
+  }));
+
+
+  /* FUNCTION */
+  // const [inputs, setInputs] = useState({
+  //   firstName: "",
+  //   lastName: "",
+  //   email: "",
+  //   password: "",
+  // });
+
+  // const [err, setError] = useState(null);
+
+  // const navigate = useNavigate();
+
+  // const handleChange = (e) => {
+  //   setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  // };
+
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   try {
+  //     await axios.post("http://localhost:8800/api/auth/register", inputs);
+  //     navigate("/login");
+  //   } catch (err) {
+  //     setError(err.response.data);
+  //   }
+  // };
+
   return (
-    <div>Register</div>
+    <CustomBox>
+      <FormBox>
+      <h1>Register</h1>
+        <input
+          required
+          type="text"
+          placeholder="First Name"
+          name="firstName"
+          // onChange={handleChange}
+        />
+        <input
+          required
+          type="text"
+          placeholder="Last Name"
+          name="lastName"
+          // onChange={handleChange}
+        />
+        <input
+          required
+          type="email"
+          placeholder="Email"
+          name="email"
+          // onChange={handleChange}
+        />
+        <input
+          required
+          type="password"
+          placeholder="Password"
+          name="password"
+          // onChange={handleChange}
+        />
+        <input
+          required
+          type="password"
+          placeholder="Confirm Password"
+          name="password"
+          // onChange={handleChange}
+        />
+        {/* <button onClick={handleSubmit}>Register</button> */}
+        <CustomButton
+          backgroundColor="#0F1B4C"
+          color="#fff"
+          buttonText="Register"
+          heroBtn={true}
+        />
+        {/* {err && <p>{err}</p>} */}
+        <span>
+          Do you have an account? <Link to="/login">Login</Link>
+        </span>
+      </FormBox>
+    </CustomBox>
   )
 }
 
